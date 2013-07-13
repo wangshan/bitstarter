3caa93c66fec009870b72460f53f256d8a047288
Shan Wang <shan.2.wang@gmail.com>
2013-07-13 19:53:23 +0000

    modified web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 1000!');
+  var greeting = fs.readFileSync('index.html').toString();
+  response.send(greeting);
 });
 
 var port = process.env.PORT || 5000;
